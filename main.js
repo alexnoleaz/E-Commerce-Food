953213adbf0606f4ca213fe57a0ab2fcc81a9ab0
Alexander Nole <cnole_azabache@outlook.com>
2022-11-22 04:20:17 -0500

    Primer avance de E-Commerce

---
+++ main.js
@@ -0,0 +1,6 @@
+// Activa la barra de busqueda
+let formSearch = document.querySelector('.form-search');
+
+document.querySelector('#btn-search').onclick = () =>{
+  formSearch.classList.toggle('active');
+}
