2ceb0a46683a1a985bd2608c5e4b767604f5df5d
Alexander Nole <cnole_azabache@outlook.com>
2022-11-23 01:24:19 -0500

    Implementacion de navbar a menu desplegable

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 // Activa la barra de busqueda
-let formSearch = document.querySelector('.form-search');
+let formSearch = document.querySelector(".form-search");
+document.querySelector("#btn-search").onclick = () => {
+  formSearch.classList.toggle("active");
+  navbar.classList.remove("active");
+};
 
-document.querySelector('#btn-search').onclick = () =>{
-  formSearch.classList.toggle('active');
-}
+// NavBar block
+let navbar = document.querySelector(".navbar");
+document.querySelector("#btn-bars").onclick = () => {
+  navbar.classList.toggle("active");
+  formSearch.classList.remove("active");
+};
+
+// others
+window.onscroll = () => {
+  formSearch.classList.remove("active");
+  navbar.classList.remove("active");
+};
